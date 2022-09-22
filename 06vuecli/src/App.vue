440730c87d1d4ac9190d4541f450657a2486ef15
Chiaki-Suzuki <c.suzuki.eba@gmail.com>
2022-09-22 16:39:06 +0900

    VueCLI エラー箇所修正

--- 06vuecli/src/App.vue
+++ 06vuecli/src/App.vue
@@ -75,7 +75,7 @@ export default {
       firstSun: ''
     }
   },
-  mounted: function() {
+  created: function() {
     // カレンダー作成
     let date = new Date();
     let year = date.getFullYear();
@@ -93,7 +93,7 @@ export default {
 
     // 空の配列に当月の日付をすべて格納したい
     let len = endDay.getDate();
-    for (i = 1; i <= len; i++) {
+    for (let i = 1; i <= len; i++) {
       this.calendar.push(i)
     }
 
@@ -102,8 +102,8 @@ export default {
     -------------------------*/
     // 最初の週だけの曜日を繰り返す
     let startDayWeek = startDay.getDay();
-    for (i = 0; i < 1; i++){
-      for (j = startDayWeek; j < weekArray.length; j++) {
+    for (let i = 0; i < 1; i++){
+      for (let j = startDayWeek; j < weekArray.length; j++) {
         this.week.push(weekArray[j]);
       }
     }
@@ -111,8 +111,8 @@ export default {
     // 最初と最後の週以外の曜日を繰り返す
     let firstWeek = (weekArray.length - startDayWeek);
     let row = Math.floor((len - firstWeek) / weekArray.length);
-    for (i = 0; i < row; i++) {
-      for (j = 0; j < weekArray.length; j++) {
+    for (let i = 0; i < row; i++) {
+      for (let j = 0; j < weekArray.length; j++) {
         this.week.push(weekArray[j])
       }
     }
@@ -121,7 +121,7 @@ export default {
     let lastWeek = endDay.getDay();
     if (lastWeek === 6) {
     } else {
-      for (i = 0; i <= lastWeek; i++) {
+      for (let i = 0; i <= lastWeek; i++) {
         this.week.push(weekArray[i])
       }
     }
@@ -134,20 +134,33 @@ export default {
       this.firstSat = (6 - startDayWeek) + 1;
       this.firstSun = this.firstSat + 1;
     }
+  },
+  mounted: function() {
+    // 当月の情報を取得
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+
+    // 今月の終了日を取得
+    let endDay = new Date(year, month, 0)
+
+    // 今月何日あるか
+    let len = endDay.getDate();
+
     /*-------------------------
       カレンダー表示
     -------------------------*/
     // 15人分の日数分のカラムを表示
     let column = document.querySelectorAll('tr.person')
-    for (i = 0; i < 15; i++) {
-      for (j = 0; j < len; j++) {
+    for (let i = 0; i < 15; i++) {
+      for (let j = 0; j < len; j++) {
         column[i].insertAdjacentHTML('beforeend', '<td></td>')
       }
     }
 
     // 土日に色を付けたい
     let holiColumn = document.querySelectorAll('tr.week th');
-    for (i = 0; i < holiColumn.length; i++){
+    for (let i = 0; i < holiColumn.length; i++){
       if (holiColumn[i].innerHTML === '土') {
         holiColumn[i].classList.add('sat');
       } else if (holiColumn[i].innerHTML === '日') {
@@ -157,27 +170,30 @@ export default {
 
     // 日付の方も
     let dateColumn = document.querySelectorAll('tr.date th');
-    for (i = this.firstSat; i < dateColumn.length; i+=7) {
+    for (let i = this.firstSat; i < dateColumn.length; i+=7) {
       dateColumn[i].classList.add('sat');
     }
-    for (i = this.firstSun; i < dateColumn.length; i+=7) {
+    for (let i = this.firstSun; i < dateColumn.length; i+=7) {
       dateColumn[i].classList.add('sun');
     }
 
     // シフト表は土日をグレーにする
-    for (i = 0; i < 15; i++) {
+    for (let i = 0; i < 15; i++) {
       let personColumn = document.querySelectorAll('tr.person');
       let shiftColumn = personColumn[i].querySelectorAll('td');
 
-      for (j = this.firstSat; j < dateColumn.length; j += 7) {
+      for (let j = this.firstSat; j < dateColumn.length; j += 7) {
         shiftColumn[j - 1].classList.add('gray');
       }
-      for (j = this.firstSun; j < dateColumn.length; j += 7) {
+      for (let j = this.firstSun; j < dateColumn.length; j += 7) {
         shiftColumn[j - 1].classList.add('gray');
       }
     }
   },
   methods: {
+    color: function() {
+
+    },
     createShift: function () {
       /*-------------------------
         当月の情報を取得
@@ -199,19 +215,19 @@ export default {
 
       // 配列をシャッフル
       let weekArray = [];
-      for (i = 0; i < weeklen + 1; i++) {
+      for (let i = 0; i < weeklen + 1; i++) {
         let shuffleArray = this.shuffle(numArray);
         weekArray.push(shuffleArray)
       }
 
       // 1列を曜日ごとに分割
       let week = [];
-      for (i = 0; i < weeklen + 1; i++) {
+      for (let i = 0; i < weeklen + 1; i++) {
         this.splitRow(weekArray[i], week);
       }
 
       // １つの曜日内に同じ数値がないように重複判定
-      for (x = 0; x < week.length; x += 5){
+      for (let x = 0; x < week.length; x += 5){
         while (this.confDup(week[x]) | this.confDup(week[x + 1]) | this.confDup(week[x + 2]) | this.confDup(week[x + 3]) | this.confDup(week[x + 4])) {
           // 再度配列をシャッフル
           let shuffleNewArray = this.shuffle(numArray);
@@ -227,12 +243,11 @@ export default {
         }
       }
 
-      console.log(week)
       // 曜日ごとの出勤日を赤く染める
       let num = 1;
-      for (i = (7 - this.firstSun); i < week.length; i++) {
+      for (let i = (7 - this.firstSun); i < week.length; i++) {
         // 土日を除く
-        for (j = this.firstSat; j <= len; j+=7) {
+        for (let j = this.firstSat; j <= len; j+=7) {
           if (num === j){
             num = num + 1;
             num = num + 1;
@@ -256,7 +271,7 @@ export default {
     },
     // 1列を曜日ごとに分割
     splitRow: function (shuffleArray, week) {
-      for (z = 0; z < 30; z += 6) {
+      for (let z = 0; z < 30; z += 6) {
         let weekDay = shuffleArray.slice(z, (z + 6));
         week.push(weekDay)
       }
@@ -266,7 +281,7 @@ export default {
       let column = document.querySelectorAll(`tr.person td:nth-of-type(${num})`);
       let personColumn = Array.from(column);
 
-      for (x = 0; x < array.length; x++) {
+      for (let x = 0; x < array.length; x++) {
         personColumn[array[x]].classList.add('red');
       }
     },
@@ -278,7 +293,7 @@ export default {
     resetShift: function () {
       // シフト表をリセット
       let redColumn = document.querySelectorAll('tr.person td.red');
-      for (i = 0; i < redColumn.length; i++) {
+      for (let i = 0; i < redColumn.length; i++) {
           redColumn[i].classList.remove('red');
       }
     }
@@ -312,6 +327,10 @@ ul, ol {
   list-style-type: none;
   margin: 0;
   padding: 0;
+}
+
+table {
+  border-collapse: collapse;
 }
 
 /*-------------------------
@@ -386,6 +405,7 @@ button:hover {
   background-color: #ccc;
   border-radius: 3px;
   box-shadow: 0 0 10px #eee;
+  margin: 0 0 0 10px;
 }
 
 .reset:hover {
